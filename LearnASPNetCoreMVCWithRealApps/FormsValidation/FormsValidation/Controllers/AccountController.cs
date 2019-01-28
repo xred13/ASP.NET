44f0186765c2cfdf2fd7629b0f3b0f0d44939053
Fábio Sfendrych Ramos <fabio.ramos10@hotmail.com>
2019-01-28 21:21:03 +0100

    Form validation with regular expressions (regex)

---
+++ LearnASPNetCoreMVCWithRealApps/FormsValidation/FormsValidation/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FormsValidation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormsValidation.Controllers
+{
+    [Route("account")]
+    public class AccountController : Controller
+    {
+        [Route("")]
+        [Route("~/")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View("Index", new Account());
+        }
+
+        [HttpPost]
+        public IActionResult Save(Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.account = account;
+                return View("Success");
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ LearnASPNetCoreMVCWithRealApps/FormsValidation/FormsValidation/Models/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormsValidation.Models
+{
+    public class Account
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(10)]
+        public string Username { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(10)]
+        [RegularExpression("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})")]
+        public string Password { get; set; }
+
+        [Required]
+        [Range(18,50)]
+        public int Age { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Url]
+        public string Website { get; set; }
+    }
+}
