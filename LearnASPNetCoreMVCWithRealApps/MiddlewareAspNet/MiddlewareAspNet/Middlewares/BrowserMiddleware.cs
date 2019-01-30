cc28403badaa0dcb7989188c47020ee13d6742a1
Fábio Sfendrych Ramos <fabio.ramos10@hotmail.com>
2019-01-30 19:20:36 +0100

    Middleware

---
+++ LearnASPNetCoreMVCWithRealApps/MiddlewareAspNet/MiddlewareAspNet/Middlewares/BrowserMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddlewareAspNet.Middlewares
+{
+    public class BrowserMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public BrowserMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            var userAgent = httpContext.Request.Headers["User-Agent"].ToString();
+            var ipAddress = httpContext.Connection.RemoteIpAddress.ToString();
+            var url = httpContext.Request.Path;
+            Debug.WriteLine("User Agent: " + userAgent);
+            Debug.WriteLine("IP: " + ipAddress);
+            Debug.WriteLine("Url: " + url);
+            return _next(httpContext);
+        }
+
+        
+    }
+}
---
+++ LearnASPNetCoreMVCWithRealApps/MiddlewareAspNet/MiddlewareAspNet/Middlewares/DateLogMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddlewareAspNet.Middlewares
+{
+    public class DateLogMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public DateLogMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            Debug.WriteLine("Date: " + DateTime.Now.ToLongDateString());
+            return _next(httpContext);
+        }
+    }
+}
